2488692a7a4df61835e5ba756fca94f79240890c
mateus <mateusweslley14@gmail.com>
2025-05-14 21:51:16 -0300

    initial commit

---
+++ backend/src/index.js
@@ -0,0 +1,41 @@
+import express from "express";
+import cors from "cors";
+import { Mongo } from "./database/mongo.js";
+import { config } from "dotenv";
+import authRouter from "./auth/auth.js";
+
+config();
+// Load environment variables from .env file
+
+async function main() {
+  const hostname = "localhost";
+  const port = 3000;
+
+  const app = express();
+
+  const mongoConnection = await Mongo.connect({
+    mongoConnectionString: process.env.MONGO_CS,
+    mongoDbName: process.env.MONGO_DB_NAME,
+  });
+
+  console.log(mongoConnection);
+
+  app.use(express.json());
+  app.use(cors());
+
+  app.get("/", (req, res) => {
+    res.send({
+      success: true,
+      statusCode: 200,
+      body: "Welcome to the MyGastronomy",
+    });
+  });
+
+  app.use("/auth", authRouter);
+
+  app.listen(port, () => {
+    console.log(`Server running on: http://${hostname}:${port}`);
+  });
+}
+
+main();
