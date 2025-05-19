7cfb353e44df0346df51b5a8dbe217168bd8269a
mateus <mateusweslley14@gmail.com>
2025-05-19 18:47:41 -0300

    Implement deleteUser and updateUser methods in UsersDataAccess for user management

--- backend/src/dataAccess/users.js
+++ backend/src/dataAccess/users.js
@@ -11,7 +11,46 @@ export default class UsersDataAccess {
     return result;
   }
 
-  async deleteUser() {}
+  async deleteUser(userId) {
+    const result = await Mongo.db
+      .collection(collectionName)
+      .findOneAndDelete({ _id: new ObjectId(userId) });
 
-  async updateUser() {}
+    return result;
+  }
+
+  async updateUser(userId, userData) {
+    if (userData.password) {
+      const salt = crypto.randomBytes(16);
+
+      crypto.pbkdf2(
+        userData.password,
+        salt,
+        310000,
+        16,
+        "sha256",
+        async (error, hashedPassword) => {
+          if (error) {
+            throw new Error("Error during hashing password");
+          }
+          userData = { ...userData, password: hashedPassword, salt };
+
+          const result = await Mongo.db
+            .collection(collectionName)
+            .findOneAndUpdate(
+              { _id: new ObjectId(userId) },
+              { $set: userData }
+            );
+
+          return result;
+        }
+      );
+    } else {
+      const result = await Mongo.db
+        .collection(collectionName)
+        .findOneAndUpdate({ _id: new ObjectId(userId) }, { $set: userData });
+
+      return result;
+    }
+  }
 }
